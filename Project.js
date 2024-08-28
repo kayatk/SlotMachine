c92e29e4e67d9c57b86157d334c39caff4881a86
kayatk <thillaikayatk@gmail.com>
2024-08-28 10:38:25 +0200

    NoOf Lines to bet function  definition

--- Project.js
+++ Project.js
@@ -1,17 +1,33 @@
 const prompt = require("prompt-sync")()
 
+//To Get Deposit Amount From User
 const deposit =() => {
-    let flag = true
-    while(flag == true){
+    while(true){
         const  depositAmount = prompt("Enter the Deposit Amount: ")
         const numberDepositAmount =parseFloat(depositAmount)
         if(isNaN(numberDepositAmount) || (numberDepositAmount <=0)){
             console.log("You Have Entered Wrong amount. Please Try Again !")    
         }else{
-            flag =false
             return numberDepositAmount
         }
 }
 }
 
-deposit()
+
+//To Get Number Lines From User
+const noOflines = () => {
+    while(true){
+    const noLines = prompt("Enter the Number of Lines to Bet: ")
+    const lines =parseInt(noLines)
+
+    if(isNaN(lines) || (lines <= 0)){
+        console.log("You Have Entered Wrong amount. Please Try Again !")  
+    }
+    else{
+        return lines
+    }
+}
+}
+
+const depositedAmount =deposit()
+const noOfLines=noOflines()
