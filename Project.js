359e48a0720da601eb7aa8d27e53224302364175
kayatk <thillaikayatk@gmail.com>
2024-08-28 10:24:04 +0200

    deposit function definition

--- Project.js
+++ Project.js
@@ -1,13 +1,17 @@
 const prompt = require("prompt-sync")()
 
 const deposit =() => {
-    const  depositAmount = prompt("Enter the Deposit Amount: ")
-    const numberDepositAmount =parseFloat(depositAmount)
-
-    if(isNaN(numberDepositAmount) || (numberDepositAmount <=0)){
-        
-    }
-
+    let flag = true
+    while(flag == true){
+        const  depositAmount = prompt("Enter the Deposit Amount: ")
+        const numberDepositAmount =parseFloat(depositAmount)
+        if(isNaN(numberDepositAmount) || (numberDepositAmount <=0)){
+            console.log("You Have Entered Wrong amount. Please Try Again !")    
+        }else{
+            flag =false
+            return numberDepositAmount
+        }
+}
 }
 
 deposit()
